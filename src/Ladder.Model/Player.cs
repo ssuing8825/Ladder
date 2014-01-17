c4ac86e30314316a3f54cc5469c6bb512df7e7b3
Steven Suing <Steven.suing@gmail.com>
2014-01-17 18:04:03 -0500

    Initial Commit

---
+++ src/Ladder.Model/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladder.Model
+{
+    public class Player
+    {
+        public static readonly string NodeTypeKey = "PERSON";
+
+        public string Name { get; set; }
+        public string NodeType
+        {
+            get { return NodeTypeKey; }
+        }
+    }
+}
---
+++ src/Ladder.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo4jClient;
+using Neo4jClient.Cypher;
+
+namespace Ladder.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private GraphClient client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            client = new GraphClient(new Uri("http://localhost.:7474/db/data"));
+            client.Connect();
+        }
+
+        [TestMethod]
+        public void CreateData()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void ClearDatabase()
+        {
+            var query = client
+               .Cypher
+               .Start(new { n = All.Nodes })
+               .Match("n-[r]-()")
+               .Delete("n, r");
+
+            query.ExecuteWithoutResults();
+
+            query = client
+            .Cypher
+            .Start(new { n = All.Nodes })
+            .Delete("n");
+
+            query.ExecuteWithoutResults();
+
+        }
+    }
+}
